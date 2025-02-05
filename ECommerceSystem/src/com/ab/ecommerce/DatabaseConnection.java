3d3dd30192987bfdc49344b3609b626139cd106d
AbdullahC4J <abdullah.lvxt@gmail.com>
2025-02-05 15:54:34 +0200

    update project structure and create core classes

--- ECommerceSystem/src/com/ab/ecommerce/DatabaseConnection.java
+++
@@ -1,4 +0,0 @@
-package com.ab.ecommerce;
-
-public class DatabaseConnection {
-}
--- ECommerceSystem/src/com/ab/ecommerce/Shop.java
+++
@@ -1,8 +0,0 @@
-package com.ab.ecommerce;
-
-public class Shop {
-
-    public static void main(String[] args) {
-        
-    }
-}
